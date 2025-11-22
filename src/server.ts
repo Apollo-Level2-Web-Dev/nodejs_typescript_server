419070a1cea1776a55863d56b9f3be97c8d94dcb
shafayathub <shafayat.ms@gmail.com>
2025-11-23 03:59:58 +0600

    post method added

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,6 @@ import "./routes";
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
-    console.log("server is running...");
-
     const method = req.method?.toUpperCase() || "";
     const path = req.url || "";
 
@@ -25,31 +23,6 @@ const server: Server = http.createServer(
         })
       );
     }
-    // if (req.url == "/api/users" && req.method == "POST") {
-    //   const user = {
-    //     id: 1,
-    //     name: "alice",
-    //   };
-    //   res.writeHead(200, { "content-type": "application/json" });
-    //   res.end(JSON.stringify(user));
-
-    // let body = "";
-
-    // //listen for data chunk
-    // req.on("data", (chunk) => {
-    //   body += chunk.toString(); // convert buffer to string
-    // });
-
-    // req.on("end", () => {
-    //   try {
-    //     const parseBody = JSON.parse(body);
-    //     console.log(parseBody);
-    //     console.log("catching current changes");
-    //     res.end(JSON.stringify(parseBody));
-    //   } catch (err: any) {
-    //     console.log(err?.message);
-    //   }
-    // });
   }
 );
 
